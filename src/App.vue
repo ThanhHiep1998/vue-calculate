26095d2d0ac7e807f593a5e561f02566f9dc97c4
ThanhHiep1998 <hothanhhiepc5@gmail.com>
2022-12-01 16:00:58 +0700

    Push 1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+import Calculator from './components/Calculator.vue'
+export default {
+  name: 'app',
+  components: {
+    Calculator
+  }
+}
+
+</script>
+
+<template>
+  <div id="app">
+    <Calculator />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
